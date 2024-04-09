1f4a7634f1ddc5e67eb40233170370bd81080203
This_is_dhanu <126862087+dhanuska-yowas@users.noreply.github.com>
2024-04-09 21:51:04 +0530

    Add files via upload

---
+++ drawings.php
@@ -0,0 +1,183 @@
+<?php
+
+include_once("./nav.html");
+?>
+
+
+<html>
+  <title>Construction Drawings </title>
+<head>
+    <link rel="stylesheet" href="drawings.css">
+</head>
+
+<body>
+    <section class="drawings">
+        <h2 style="color:brown font-size:40px text-align:center">Construction Drawings</h2>
+        <p>Click on first image to view a larger version. other images are display after buy the drawing</p>
+    
+    
+   
+        <div class="image-gallery"> 
+        
+          <div class="image-container">
+          <a href="image/drawing1.jpeg" target="_blank"><img src="image/drawing1.jpeg" alt="Image 1"></a>
+          <a href="image/drawing30.jpeg" target="_blank"><img src="image/drawing30.jpeg" alt="Image 2" style="display: none;"></a>
+           <a href="image/drawing40.jpeg" target="_blank"><img src="image/drawing40.jpeg" alt="Image 3" style="display: none;"></a>
+            <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+          </div>
+    
+          
+          <div class="image-container">
+          <a href="image/drawing2.jpeg" target="_blank"><img src="image/drawing2.jpeg" alt="Image 4"></a>
+           <img src="image/drawing31.jpeg" alt="Image 5" style="display: none;">
+           <img src="image/drawing41.jpeg" alt="Image 6" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+          </div>
+    
+    
+          <div class="image-container">
+          <a href="image/drawing3.jpeg" target="_blank"><img src="image/drawing3.jpeg" alt="Image 4"></a>
+           <img src="image/drawing32.jpeg" alt="Image 2" style="display: none;">
+           <img src="image/drawing42.jpeg" alt="Image 3" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+          </div>
+    
+          <div class="image-container">
+          <a href="image/drawing4.jpeg" target="_blank"><img src="image/drawing4.jpeg" alt="Image 4"></a>
+           <img src="image/drawing33.jpeg" alt="Image 2" style="display: none;">
+           <img src="image/drawing44.jpeg" alt="Image 3" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+          </div>
+       
+          <div class="image-container">
+          <a href="image/drawing5.jpeg" target="_blank"><img src="image/drawing5.jpeg" alt="Image 4"></a>
+           <img src="image/drawing34.jpeg" alt="Image 2" style="display: none;">
+           <img src="image/drawing44.jpeg" alt="Image 3" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+          </div>
+
+          <div class="image-container">
+          <a href="image/drawing6.jpeg" target="_blank"><img src="image/drawing6.jpeg" alt="Image 4"></a>
+           <img src="image/drawing35.jpeg" alt="Image 2" style="display: none;">
+           <img src="image/drawing45.jpeg" alt="Image 3" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+          </div>
+
+          <div class="image-container">
+          <a href="image/drawing7.jpeg" target="_blank"><img src="image/drawing7.jpeg" alt="Image 4"></a>
+           <img src="image/drawing36.jpeg" alt="Image 2" style="display: none;">
+           <img src="image/drawing46.jpeg" alt="Image 3" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+          </div>
+
+          <div class="image-container">
+          <a href="image/drawing8.jpeg" target="_blank"><img src="image/drawing8.jpeg" alt="Image 4"></a>
+           <img src="image/drawing37.jpeg" alt="Image 2" style="display: none;">
+           <img src="image/drawing47.jpeg" alt="Image 3" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+          </div>
+
+          <div class="image-container">
+          <a href="image/drawing9.jpeg" target="_blank"><img src="image/drawing9.jpeg" alt="Image 4"></a>
+           <img src="image/drawing38.jpeg" alt="Image 2" style="display: none;">
+           <img src="image/drawing48.jpeg" alt="Image 3" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+</div>
+
+            <div class="image-container">
+            <a href="image/drawing10.jpeg" target="_blank"><img src="image/drawing10.jpeg" alt="Image 4"></a>
+           <img src="image/drawing39.jpeg" alt="Image 2" style="display: none;">
+           <img src="image/drawing49.jpeg" alt="Image 3" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+</div>
+
+
+            <div class="image-container">
+            <a href="image/drawing11.jpeg" target="_blank"><img src="image/drawing11.jpeg" alt="Image 4"></a>
+           <img src="image/drawing17.jpeg" alt="Image 2" style="display: none;">
+           <img src="image/drawing22.jpeg" alt="Image 3" style="display: none;">
+           <div>
+              <button onclick="previousImage()" class="arrow-button prev">‹</button>
+              <button onclick="nextImage()" class="arrow-button next">›</button>
+              <a href="payment.html"><button>Download</button></a>
+            </div>
+          </div>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.2/FileSaver.min.js"></script>
+
+
+          <script >
+            let currentImageIndex = 0;
+            let length=3;
+              const images = document.querySelectorAll('.image-container img');
+
+              function showImage(index) {
+                images.forEach((img, idx) => {
+                  if (idx === index) {
+                    img.style.display = 'inline-block';
+                  } else {
+                    img.style.display = 'none';
+                  }
+                });
+              }
+
+              function nextImage() {
+                currentImageIndex = (currentImageIndex + 1) % images.length;
+                showImage(currentImageIndex);
+              
+               
+              }
+
+              function previousImage() {
+                currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+                showImage(currentImageIndex);
+              }
+
+
+
+
+             
+             
+          </script>
+</body>
+</html>
+
---
+++ veihcle.php
@@ -0,0 +1,32 @@
+<?php
+
+include_once("./nav.html");
+
+?>
+
+<html>
+    <head>
+        <title>Hiring Vehicle</title>
+       
+
+        <style>
+            body{
+                    background-image: linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.5)),url(image/veihle.jpg);
+                background-repeat: no-repeat;
+                background-size: cover;
+                }
+
+                h1,h2{
+                    color: aliceblue;
+                    text-align: center;
+                    font-family: 'Times New Roman', Times, serif;
+                    font-size: 80px;
+                    padding-top: 50px;
+                }
+</style>
+    </head>
+    <body>
+        <h1>For hiring veihcle contact us</h1>
+        <h2>076 - 56 88 342</h2>
+    </body>
+</html>
